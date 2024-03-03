cee67c9e46edc834223647fe59161e05797f79d5
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-03-03 00:26:47 -0600

    Refactors

--- apps/docs/scripts/create-docs.ts
+++ apps/docs/scripts/create-docs.ts
@@ -2,25 +2,29 @@ import {copy} from 'fs-extra';
 import {readdir} from 'node:fs/promises';
 
 const paths = {
-  components: '../../libraries/ui/components',
-  primitives: '../../libraries/exo/src/components',
-  hooks: '../../libraries/exo/src/hooks',
+  components: ['../../libraries/ui/components', './pages/components'],
+  primitives: ['../../libraries/exo/src/components', './pages/primitives'],
+  hooks: ['../../libraries/exo/src/hooks', './pages/hooks'],
 };
 
+const copydocs = (files: string[], paths: string[]) => files
+  .flat(1)
+  .filter(f => f.endsWith('.mdx'))
+  .map(async path => {
+    await copy(
+      `${paths[0]}/${path}`,
+      `${paths[1]}/${path.split('/').slice(0, -1).join('/')}.mdx`
+    );
+  });
+
 const [components, primitives, hooks] = [
-  await readdir(paths.components, {recursive: true}),
-  await readdir(paths.primitives, {recursive: true}),
-  await readdir(paths.hooks, {recursive: true}),
+  await readdir(paths.components[0], {recursive: true}),
+  await readdir(paths.primitives[0], {recursive: true}),
+  await readdir(paths.hooks[0], {recursive: true}),
 ];
 
 await Promise.all([
-  copydocs(components, paths.components, './pages/components'),
-  copydocs(primitives, paths.primitives, './pages/primitives'),
-  copydocs(hooks, paths.hooks, './pages/hooks'),
+  copydocs(components, paths.components),
+  copydocs(primitives, paths.primitives),
+  copydocs(hooks, paths.hooks),
 ].flat(1));
-
-function copydocs(files: string[], from: string, to: string) {
-  files.flat(1).filter(f => f.endsWith('.mdx')).map(async path => {
-    await copy(`${from}/${path}`, `${to}/${path.split('/').slice(0, -1).join('/')}.mdx`);
-  });
-}
