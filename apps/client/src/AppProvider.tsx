fc478136287631c09a7fae5f49539433dee079da
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-03-25 07:28:57 +0900

    Update AppProvider.tsx

--- apps/client/src/AppProvider.tsx
+++ apps/client/src/AppProvider.tsx
@@ -1,13 +1,13 @@
 import {useEffect} from 'react';
-import {Appearance, StatusBar} from 'react-native';
 import {I18nProvider} from '@lingui/react';
-import {BootSplash} from 'react-exo/bootsplash';
-import {SafeAreaProvider} from 'react-exo/safearea';
+import {Appearance, StatusBar} from 'react-native';
 import {GestureHandlerRootView} from 'react-exo/gesturehandler';
+import {SafeAreaProvider} from 'react-exo/safearea';
+import {BootSplash} from 'react-exo/bootsplash';
 import {loadLocale, i18n} from 'common/i18n';
-import {useLocale} from 'modules/settings/hooks/useLocale';
-import {useScheme} from 'modules/settings/hooks/useScheme';
 import {isNative} from 'common/utils/platform';
+import {useScheme} from 'modules/settings/hooks/useScheme';
+import {useLocale} from 'modules/settings/hooks/useLocale';
 
 export interface AppProviderProps {
   children: React.ReactNode;
