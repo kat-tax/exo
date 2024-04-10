fe8facc4d6772cbb01faf28e763d10527ae589c2
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-04-10 18:11:50 +0800

    Update docs

--- toolkit/vocs/vocs.config.ts
+++ toolkit/vocs/vocs.config.ts
@@ -5,6 +5,7 @@ import viteDocs from 'builder/vite.docs';
 export default defineConfig({
   ...docsConfig,
   rootDir: './',
+  ogImageUrl: 'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
   twoslash: {
     compilerOptions: {
       moduleResolution: 100,
