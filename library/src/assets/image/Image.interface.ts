dbbbf543024c49c30919266d8eff97277b387fea
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-04-11 18:54:06 +0800

    Docs

--- library/src/assets/image/Image.interface.ts
+++ library/src/assets/image/Image.interface.ts
@@ -1,32 +1,35 @@
-import type {ImageStyle, ImageResizeMode} from 'react-native';
+import type {ImageStyle, StyleProp} from 'react-native';
 
 export type ImageComponent = (props: ImageProps) => JSX.Element;
 
 export interface ImageProps {
   /** URL of the image */
   url: string,
+  /** Width of the image */
+  width?: number,
+  /** Height of the image */
+  height?: number,
   /** Style of the image */
-  style: ImageStyle,
-  /** Blurhash of the image (base64 encoded) */
-  blurhash?: string,
-  /** Thumbhash of the image (base64 encoded) */
+  style?: StyleProp<ImageStyle> | undefined,
+  /** A base64 encoded thumbhash of the image */
   thumbhash?: string,
-  /** Image to show when the image fails to load, pass blurhash, thumbhash or base64 encoded image */
+  /** A base64 encoded image to show when the image fails to load */
   failureImage?: string,
   /** Resize mode of the image */
-  resizeMode?: ImageResizeMode,
+  resizeMode?: 'cover' | 'contain' | 'stretch' | 'center';
   /** Duration of the transition animation in seconds, defaults to 0.75 */
   transitionDuration?: number,
   /** Show activity indicator while loading, overrides placeholder. Defaults to false */
   showActivityIndicator?: boolean,
   /** Enable progressive loading, defaults to false */
   progressiveLoadingEnabled?: boolean,
-  /** Rounds the image into a circle, defaults to false */
-  rounded?: boolean,
-  /** Cache [policy](https://kean-docs.github.io/nuke/documentation/nuke/imagepipeline).
-   * Defaults to 'memory'. 'discWithCacheControl' will cache the image in the disc and
-   * use the cache control headers to determine if the image should be re-fetched.
+  /** Border radius of the image */
+  borderRadius?: number,
+  /** Caching policy:
+   * 'discWithCacheControl' caches the image in the disc and uses the cache control headers.
    * 'discNoCacheControl' will cache the image in the disc and never re-fetch it.
+   * 'memory' uses the default platform caching policy with no explicit saving to disk.
+   * @default memory
    */
   cachePolicy?: 'memory' | 'discWithCacheControl' | 'discNoCacheControl',
   /** Callback for when the image loads successfully */
--- library/src/assets/image/Image.ios.tsx
+++
@@ -1,8 +0,0 @@
-import {FasterImageView as ImageBase} from '@candlefinance/faster-image';
-import type {ImageComponent, ImageProps} from './Image.interface';
-
-export const Image: ImageComponent = (props: ImageProps) => {
-  return (
-    <ImageBase {...props}/>
-  );
-}
