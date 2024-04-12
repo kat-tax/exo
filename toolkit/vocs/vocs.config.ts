e4faaeca1658d9079c688e20dd0d57959c04338f
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-04-12 19:23:53 +0800

    Docs

--- toolkit/vocs/vocs.config.ts
+++ toolkit/vocs/vocs.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from 'vocs';
+// import {themes} from 'design/theme';
 import viteDocs from 'builder/vite.docs';
 import sidebar from 'config/docs.sidebar';
 import config from 'config';
@@ -6,34 +7,36 @@ import config from 'config';
 export default defineConfig({
   title: config.APP_NAME,
   sponsors: [],
-  socials: [{
-    icon: 'github',
-    link: config.LINK_GITHUB,
-  },
-  {
-    icon: 'discord',
-    link: config.LINK_DISCORD,
-  },
-  {
-    icon: 'x',
-    link: config.LINK_X,
-  }],
+  socials: [
+    {
+      icon: 'github',
+      link: config.LINK_GITHUB,
+    },
+    {
+      icon: 'discord',
+      link: config.LINK_DISCORD,
+    },
+    {
+      icon: 'x',
+      link: config.LINK_X,
+    },
+  ],
   topNav: [
     {
       text: 'Storybook',
       link: config.LINK_STORYBOOK,
     },
-    { 
-      text: `v${config.LIB_VERSION}`, 
+    {
+      text: `v${config.LIB_VERSION}`,
       items: [
-        { 
-          text: 'Changelog', 
-          link: `${config.LINK_GITHUB}/blob/master/CHANGELOG.md`, 
+        {
+          text: 'Changelog',
+          link: `${config.LINK_GITHUB}/blob/master/CHANGELOG.md`,
         },
-        { 
-          text: 'License', 
-          link: `${config.LINK_GITHUB}/blob/master/LICENSE.md`, 
-        }, 
+        {
+          text: 'License',
+          link: `${config.LINK_GITHUB}/blob/master/LICENSE.md`,
+        },
       ],
     },
   ],
@@ -45,20 +48,44 @@ export default defineConfig({
           dark: 'black',
         },
       },
-    }
+    },
   },
-  font: { 
+  /*
+    theme: {
+      accentColor: {
+        light: themes.light.colors.primary,
+        dark: themes.dark.colors.primary,
+      },
+      variables: {
+        color: {
+          text: {
+            light: themes.light.colors.foreground,
+            dark: themes.dark.colors.foreground,
+          },
+          background: {
+            light: themes.light.colors.background,
+            dark: themes.dark.colors.background,
+          },
+          border: {
+            light: themes.light.colors.border,
+            dark: themes.dark.colors.border,
+          },
+        },
+      }
+    },
+  */
+  font: {
     google: config.FONT_NAME,
-  },
-  twoslash: {
-    compilerOptions: {
-      moduleResolution: 100,
-    }
   },
   vite: viteDocs({
     command: 'build',
     mode: '',
   }),
+  twoslash: {
+    compilerOptions: {
+      moduleResolution: 100,
+    }
+  },
   ogImageUrl: 'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
   rootDir: './',
   sidebar,
