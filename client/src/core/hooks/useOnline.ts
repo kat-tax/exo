a241714e29e80df9573a20bb081501b439df773f
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-04-15 18:41:18 +0800

    Online toast

--- client/src/core/hooks/useOnline.ts
+++ client/src/core/hooks/useOnline.ts
@@ -1,11 +1,27 @@
+import {t} from '@lingui/macro';
+import {useLingui} from '@lingui/react';
 import {useState, useEffect} from 'react';
 import {isOnline, suscribeOnline} from 'react-exo/device';
+import {toast} from 'react-exo/toast';
 
 export function useOnline() {
   const [online, setOnline] = useState(false);
+
+  const handleConnectivity = (online: boolean) => {
+    setOnline(online);
+    if (online) {
+      toast({title: t`You are online`, preset: 'done'});
+    } else {
+      toast({title: t`You are offline`, preset: 'error'});
+    }
+  };
+
+  useLingui();
+
   useEffect(() => {
-    isOnline().then(setOnline);
-    return suscribeOnline(setOnline);
+    isOnline().then(handleConnectivity);
+    return suscribeOnline(handleConnectivity);
   }, []);
+
   return online;
 }
