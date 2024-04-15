05bd0443d56d4e82e96911d034c54dbcbea9a2f1
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-04-15 19:38:12 +0800

    Fix online message

--- client/src/core/hooks/useOnline.ts
+++ client/src/core/hooks/useOnline.ts
@@ -5,20 +5,20 @@ import {isOnline, suscribeOnline} from 'react-exo/device';
 import {toast} from 'react-exo/toast';
 
 export function useOnline() {
-  const [online, setOnline] = useState(true);
+  const [online, setOnline] = useState(false);
 
-  const handleConnectivity = (connected: boolean) => {
-    if (connected && !online) {
+  const handleConnectivity = (connected: boolean, init?: boolean) => {
+    if (connected && !init) {
       toast({title: t`You are online`, preset: 'done'});
-    } else if (!connected && online) {
+    } else if (connected) {
       toast({title: t`You are offline`, preset: 'error'});
     }
     setOnline(connected);
-  };
+  }
 
   useLingui();
   useEffect(() => {
-    isOnline().then(handleConnectivity);
+    isOnline().then((online) => handleConnectivity(online, true));
     return suscribeOnline(handleConnectivity);
   }, []);
 
