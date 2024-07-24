8ef9587e4d5ca1aab7cfa92f81a6a599a6c97cfc
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-07-24 02:12:36 -0500

    Update MarkerDevice.tsx

--- client/src/map/base/MarkerDevice.tsx
+++ client/src/map/base/MarkerDevice.tsx
@@ -1,4 +1,5 @@
 import {Icon} from 'react-exo/icon';
+import {Motion} from 'react-exo/motion';
 import {Marker} from 'react-map-gl/maplibre';
 import {useStyles} from 'react-native-unistyles';
 
@@ -13,14 +14,22 @@ export function MarkerDevice(props: MarkerDeviceProps) {
   const {theme} = useStyles();
   return (
     <Marker {...props}>
-      <Icon
-        name="ph:map-pin"
-        size={props.isSelf ? 32 : 24}
-        color={props.online
-          ? theme.colors.primary
-          : theme.colors.secondary
-        }
-      />
+      <Motion.Pressable onPress={console.log}>
+        <Motion.View
+          initial={{scale: 1}}
+          whileTap={{scale: 0.95}}
+          whileHover={{scale: 1.1}}
+          transition={{type: 'spring', speed: 100}}>
+          <Icon
+            name="ph:map-pin"
+            size={props.isSelf ? 32 : 24}
+            color={props.online
+              ? theme.colors.primary
+              : theme.colors.mutedForeground
+            }
+          />
+        </Motion.View>
+      </Motion.Pressable>
     </Marker>
   )
 }
