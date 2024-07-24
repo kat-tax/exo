341f77779f16d39c394e0e64516c621a8fff57bf
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-07-24 02:25:11 -0500

    Bootsplash fix for web

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,16 @@
-import {useEffect} from 'react';
-import {useInitialTheme, UnistylesRuntime} from 'react-native-unistyles';
 import {StatusBar, Appearance} from 'react-native';
 import {ToastRoot} from 'react-exo/toast';
+import {useEffect} from 'react';
+import {useInitialTheme, UnistylesRuntime} from 'react-native-unistyles';
 import {useScheme} from 'settings/hooks/useScheme';
-//import Bootsplash from 'react-native-bootsplash';
+import {hideBootsplash} from 'app/utils/bootsplash';
 
 export function Layout(props: React.PropsWithChildren) {
   const [scheme] = useScheme();
   const theme = scheme === 'dark' ? 'dark' : 'light';
 
   useInitialTheme(theme);
-  
+  useEffect(hideBootsplash, []);
   useEffect(() => {
     if (scheme) {
       UnistylesRuntime.setTheme(scheme);
@@ -19,10 +19,6 @@ export function Layout(props: React.PropsWithChildren) {
       }
     }
   }, [scheme]);
-
-  useEffect(() => {
-    //Bootsplash.hide();
-  }, []);
 
   return <>
     <StatusBar barStyle={`${theme}-content`}/>
---
+++ client/src/app/utils/bootsplash.native.ts
@@ -0,0 +1,5 @@
+import Bootsplash from 'react-native-bootsplash';
+
+export function hideBootsplash() {
+  Bootsplash.hide();
+}
