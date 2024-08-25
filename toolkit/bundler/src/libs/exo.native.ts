b8a01baa3979bebf6146edcd92c509e2be206f08
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-08-24 20:51:26 -0500

    Fix CF

--- toolkit/bundler/src/libs/exo.native.ts
+++ toolkit/bundler/src/libs/exo.native.ts
@@ -6,6 +6,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(env => mergeConfig(
   nativeConfig(env),
   defineConfig({
+    esbuild: {
+      supported: {
+        'top-level-await': true,
+      },
+    },
     build: {
       outDir: './gen/native',
       sourcemap: false,
