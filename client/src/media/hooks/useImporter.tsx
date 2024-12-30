f86f80dd4d60aa02d15db719f26c35c3e291f99f
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-12-30 01:50:02 -0600

    refactor

--- client/src/media/hooks/useImporter.tsx
+++ client/src/media/hooks/useImporter.tsx
@@ -1,24 +1,30 @@
 import {FS} from 'react-exo/fs';
 import {useCallback} from 'react';
-import {getStartInDir} from 'media/utils/path';
+import {useLocation} from 'react-exo/navigation';
+import {resolve, getStartInDir} from 'media/utils/path';
 
 export function useImporter() {
+  const {pathname} = useLocation();
+
   /** Import a folder from the device */
-  const importFolder = useCallback(async (path = '.') => {
+  const importFolder = useCallback(async () => {
+    console.log('>> importFolder', resolve(pathname));
+    const pathParts = resolve(pathname);
     const perfStart = performance.now();
-    const startIn = getStartInDir(path);
-    const dir = await FS.openDirectory({id: path, startIn, mode: 'read'});
-    const copy = await FS.importDirectory(dir, path);
+    const startIn = getStartInDir(pathParts[0]);
+    const dir = await FS.openDirectory({id: pathParts[0], startIn, mode: 'read'});
+    const copy = await FS.importDirectory(dir, pathParts.join('/'));
     console.log('[fs] import dir', dir, copy, performance.now() - perfStart);
-  }, []);
+  }, [pathname]);
 
   /** Import a file from the device */
   const importFile = useCallback(async () => {
+    console.log('>> importFile', resolve(pathname));
     const perfStart = performance.now();
     const file = await FS.openFile();
     const copy = await FS.importFile(file);
     console.log('[fs] import file', file, copy, performance.now() - perfStart);
-  }, []);
+  }, [pathname]);
 
   return {importFolder, importFile};
 }
