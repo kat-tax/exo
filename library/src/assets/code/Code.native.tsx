c05d46b464f93d08c3e54d09b25bd6d2774f3f78
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-12-31 12:01:46 -0600

    fix: code on native wip

---
+++ library/src/assets/code/Code.native.tsx
@@ -0,0 +1,30 @@
+// import {createHighlighter} from 'shiki';
+// import {createOnigurumaEngine} from 'shiki/engine-oniguruma.mjs';
+// import {useEffect, useState, useRef} from 'react';
+
+// TODO: Native Shiki Engine
+// Engine: https://github.com/skiniks/react-native-shiki-engine?tab=readme-ov-file#example-implementation
+// Display: https://github.com/skiniks/react-native-shiki-engine/blob/main/example/src/components/TokenDisplay.tsx
+
+import type {CodeProps} from './Code.interface';
+
+// const highlighterInit = createHighlighter({
+//   engine: createOnigurumaEngine(import('shiki/wasm')),
+//   themes: ['dark-plus', 'light-plus'],
+//   langs: ['typescript', 'tsx'],
+// });
+
+export function Code(props: CodeProps) {
+  // const {lang, theme, children} = props;
+  // const [code, setCode] = useState('');
+  // const shiki = useRef<Awaited<typeof highlighterInit>>();
+
+  // useEffect(() => {
+  //   (async () => {
+  //     shiki.current = await highlighterInit;
+  //     setCode(shiki.current.codeToHtml(children, {lang, theme}));
+  //   })();
+  // }, [children, lang, theme]);
+
+  return null;
+}
