1414f0f039927d9e299d868bd7aad921ffe9066f
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-12-31 17:34:16 -0600

    translations

--- client/src/media/routes/ScreenBrowse.tsx
+++ client/src/media/routes/ScreenBrowse.tsx
@@ -1,19 +1,44 @@
+import {useMemo} from 'react';
 import {useLingui} from '@lingui/react/macro';
 import {useLocation} from 'react-exo/navigation';
 import {useAppContext} from 'app/hooks/useAppContext';
 import {useInitializer} from 'media/hooks/useInitializer';
+import {InitDirectory} from 'media/utils/path';
 import {resolve} from 'media/utils/path';
 import {DirHfs} from 'media/dir/DirHfs';
 import {Page} from 'app/interface/Page';
+
 
 export default function ScreenBrowse() {
   const {pathname} = useLocation();
   const {layout} = useAppContext();
   const {t} = useLingui();
 
-  const parts = resolve(pathname);
-  const name = parts[parts.length - 1] || t`Files`;
-  const base = parts.slice(1, -1).join('/') || '/';
+  const [name, path] = useMemo(() => {
+    const parts = resolve(pathname);
+    const name = parts[parts.length - 1] as InitDirectory;
+    const path = parts.slice(1, -1).join('/') || '/';
+    switch (name) {
+      case InitDirectory.Documents:
+        return [t`Documents`, path];
+      case InitDirectory.Music:
+        return [t`Music`, path];
+      case InitDirectory.Pictures:
+        return [t`Pictures`, path];
+      case InitDirectory.Videos:
+        return [t`Videos`, path];
+      case InitDirectory.Games:
+        return [t`Games`, path];
+      case InitDirectory.Books:
+        return [t`Books`, path];
+      case InitDirectory.Downloads:
+        return [t`Downloads`, path];
+      case InitDirectory.Uploads:
+        return [t`Uploads`, path];
+      default: name satisfies never;
+    }
+    return [name ?? t`Files`, path];
+  }, [pathname, t]);
 
   useInitializer();
 
@@ -22,9 +47,9 @@ export default function ScreenBrowse() {
       fullWidth
       margin="small"
       title={name}
-      message={base ?? '/'}
+      message={path}
       hasPanel={layout.hasPreviewPanel}>
-      <DirHfs path={parts.join('/')}/>
+      <DirHfs {...{path}}/>
     </Page>
   );
 }
--- client/src/media/utils/path.ts
+++ client/src/media/utils/path.ts
@@ -1,15 +1,17 @@
 import type {OpenDirectoryOptions} from 'react-exo/fs';
 
-export const INIT_DIRECTORIES = [
-  'documents',
-  'music',
-  'pictures',
-  'videos',
-  'games',
-  'books',
-  'downloads',
-  'uploads',
-] as const;
+export enum InitDirectory {
+  Documents = 'documents',
+  Music = 'music',
+  Pictures = 'pictures',
+  Videos = 'videos',
+  Games = 'games',
+  Books = 'books',
+  Downloads = 'downloads',
+  Uploads = 'uploads'
+}
+
+export const INIT_DIRECTORIES = Object.keys(InitDirectory) as (keyof typeof InitDirectory)[];
 
 export function resolve(path: string) {
   return path
