ea56277b8f4dbef3ba490a800522476c2474da62
Syntax <2079305+TheUltDev@users.noreply.github.com>
2025-01-01 16:09:39 -0600

    Update loader.native.ts

--- client/src/app/locales/loader.native.ts
+++ client/src/app/locales/loader.native.ts
@@ -4,11 +4,13 @@ import {sourceLocale} from 'config/locales';
 import type {Locales} from 'config/locales';
 
 import {messages as en} from './messages/en';
+import {messages as es} from './messages/es';
 import {messages as ru} from './messages/ru';
 import {messages as ja} from './messages/ja';
+import {messages as ko} from './messages/ko';
 import {messages as ar} from './messages/ar';
 
-const locales = {en, ru, ja, ar};
+const locales = {en, es, ru, ja, ko, ar};
 
 export {i18n, isRTL};
 
