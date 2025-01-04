df96a3853eecbf00c9bde565278453b7dd483300
Syntax <2079305+TheUltDev@users.noreply.github.com>
2025-01-04 16:37:13 -0600

    fix: hfs not updating

--- client/src/media/hooks/useDirHfs.ts
+++ client/src/media/hooks/useDirHfs.ts
@@ -1,5 +1,5 @@
 import {FS} from 'react-exo/fs';
-import {useState, useEffect} from 'react';
+import {useState, useMemo, useCallback, useEffect} from 'react';
 import {isInitDirectory} from 'media/utils/path';
 
 import type {HfsDirectoryEntry} from 'react-exo/fs';
@@ -9,35 +9,44 @@ export interface DirectoryOptions {
 }
 
 export function useDirHfs(path: string, options?: DirectoryOptions) {
+  const hfsImpl = useMemo(() => FS.init('fs'), []);
   const [entries, setEntries] = useState<HfsDirectoryEntry[]>([]);
   const {showHidden} = options || {};
 
+  const refresh = useCallback(async () => {
+    const hfs = await hfsImpl;
+    if (!hfs) return;
+    const entries: HfsDirectoryEntry[] = [];
+    const dirPath = path || '.';
+    for await (const entry of hfs.list?.(dirPath) ?? []) {
+      if (entry.name.endsWith('.crswap'))
+        continue;
+      if (entry.name.startsWith('.') && !showHidden)
+        continue;
+      if (dirPath === '.' && isInitDirectory(entry.name))
+        continue;
+      entries.push(entry);
+    }
+    setEntries(entries.sort((a, b) => {
+      if (a.name.startsWith('.') && !showHidden)
+        return 1;
+      if (b.name.startsWith('.') && !showHidden)
+        return -1;
+      return a.name.localeCompare(b.name);
+    }));
+  }, [path, hfsImpl, showHidden]);
+
+  // Poll for changes in the directory
+  // TODO: replace with observer
   useEffect(() => {
-    (async () => {
-      const hfs = await FS.init('fs');
-      const entries: HfsDirectoryEntry[] = [];
-      const dirPath = path || '.';
-      for await (const entry of hfs.list?.(dirPath) ?? []) {
-        if (entry.name.endsWith('.crswap'))
-          continue;
-        if (entry.name.startsWith('.') && !showHidden)
-          continue;
-        if (dirPath === '.' && isInitDirectory(entry.name))
-          continue;
-        entries.push(entry);
-      }
-      setEntries(entries.sort((a, b) => {
-        if (a.name.startsWith('.') && !showHidden)
-          return 1;
-        if (b.name.startsWith('.') && !showHidden)
-          return -1;
-        return a.name.localeCompare(b.name);
-      }));
-    })();
-  }, [path, showHidden]);
+    refresh();
+    const interval = setInterval(refresh, 1000);
+    return () => clearInterval(interval);
+  }, [refresh]);
 
   return {
     path,
     entries,
+    refresh,
   };
 }
