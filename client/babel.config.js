508fc8f1ea323f28491101411b1f0d2bec774bfa
Syntax <2079305+TheUltDev@users.noreply.github.com>
2025-01-15 22:17:29 -0600

    chore: remove temp lib patch

--- client/babel.config.js
+++ client/babel.config.js
@@ -8,10 +8,6 @@ module.exports = {
         'tsconfig-paths-module-resolver',
         'react-exo/babel-plugin-iconify-extract',
         'react-exo/babel-plugin-iconify-transform',
-        ['@rnx-kit/babel-plugin-import-path-remapper', {
-          test: (path) => path.startsWith('react-exo/'),
-          remap: (_, path) => `${__dirname}/node_modules/react-exo/gen/native/${path}`,
-        }],
       ]
     }]
   ],
