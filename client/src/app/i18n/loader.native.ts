44a55e93b6338858e9591c9cec2acdd31b4fa16c
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-07-29 17:10:55 -0500

    Update loader.native.ts

--- client/src/app/i18n/loader.native.ts
+++ client/src/app/i18n/loader.native.ts
@@ -5,17 +5,13 @@ import {sourceLocale} from 'config/locales';
 import type {Messages} from '@lingui/core';
 import type {Locales} from 'config/locales';
 
-// Load all messages from the messages directory
 // @ts-expect-error - Available in Metro (transformer.unstable_allowRequireContext)
 const ctx = require.context('./messages', false, /\.json$/);
-const msg = new Map<Locales, Messages>();
-ctx.keys().forEach((key: string) => {
-  const lang = ctx(key) as {messages: Messages};
-  msg.set(key.replace('./', '').replace('.json', '') as Locales, lang.messages);
-});
+const dir = new Map<Locales, Messages>();
+ctx.keys().forEach((p: string) => dir.set(p.slice(2, -5) as Locales, ctx(p).messages));
 
 export {i18n, isRTL};
 
 export async function load(locale: Locales = sourceLocale) {
-  i18n.loadAndActivate({locale, messages: msg.get(locale) as Messages});
+  i18n.loadAndActivate({locale, messages: dir.get(locale) ?? {}});
 }
