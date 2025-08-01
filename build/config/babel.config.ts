652a65c3d58a2bde19aeb45bf459c4308f5d2af0
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-07-31 20:06:29 -0500

    .

--- build/config/babel.config.ts
+++ build/config/babel.config.ts
@@ -1,25 +1,29 @@
+
+import path from 'path';
+import createResolve from 'babel-plugin-tsconfig-paths-module-resolver/create-resolve';
+import type {TransformOptions} from '@babel/core';
+
 // Workaround to resolve design package imports for metro
 // This should use tsconfig path in design workspace instead
 // But the tsconfig resolver is only working in the client workspace
-const createResolve = require('babel-plugin-tsconfig-paths-module-resolver/create-resolve');
-const path = require('path');
-const _resolvePath = createResolve();
-function resolvePath(sourceFile, currentFile, opts) {
-  const result = _resolvePath(sourceFile, currentFile, opts);
+const defaultResolvePath = createResolve();
+const resolvePath = (sourceFile: string, currentFile: string, opts: any) => {
+  const result = defaultResolvePath(sourceFile, currentFile, opts);
   if (path.normalize(currentFile).includes(path.join('design', 'components'))) {
     if (result === null && sourceFile.startsWith('components/')) {
       return sourceFile.replace('components/', '../../../');
     }
   }
   return result;
-}
+};
 
-/** @type {import('@babel/core').ConfigAPI} */
-module.exports = {
+export default <TransformOptions> {
   plugins: [
     'macros',
     ['tsconfig-paths-module-resolver', {resolvePath}],
     '@lingui/babel-plugin-lingui-macro',
+    'react-exo/babel-plugin-iconify-extract',
+    'react-exo/babel-plugin-iconify-transform',
   ],
   presets: [
     '@react-native/babel-preset',
