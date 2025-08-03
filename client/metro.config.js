835df53e39b2ce6ffb798c42edd77f49783cd019
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-08-03 08:58:16 -0500

    move metro to config workspace

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,13 +1,1 @@
-const {makeMetroConfig} = require('@rnx-kit/metro-config');
-const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
-
-module.exports = makeMetroConfig({
-  // Needed for PNPM symlinks
-  resolver: {
-    resolveRequest: MetroSymlinksResolver(),
-  },
-  // Needed for Lingui imports
-  transformer: {
-    unstable_allowRequireContext: true,
-  },
-});
+module.exports = require('config/metro');
