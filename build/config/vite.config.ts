e9bd7fcac2f9f8704ac1b6eb788f0f8810a9d4b1
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-08-07 11:56:06 -0500

    Update vite.config.ts

--- build/config/vite.config.ts
+++ build/config/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(env => ({
     rollupOptions: {
       output: {
         format: 'es',
-        chunkFileNames: '[name]-[hash].js',
+        chunkFileNames: 'assets/dyn/[name]-[hash].js',
       },
       onwarn: (warning, warn) => {
         if (warning.code === 'EVAL') return;
