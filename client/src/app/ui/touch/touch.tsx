dcc73b0c6822fcb2e5d7695442592994740ecb86
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-11-07 16:38:01 -0600

    fix multi select

--- client/src/app/ui/touch/touch.tsx
+++ client/src/app/ui/touch/touch.tsx
@@ -1,6 +1,7 @@
 import {useComposedRefs} from 'app/lib/components';
 import {useTheme} from 'settings/hooks/use-theme';
 
+import type {GestureResponderEvent} from 'react-native';
 import type {TouchProps} from './touch.types';
 
 export function Touch(props: TouchProps) {
@@ -11,9 +12,9 @@ export function Touch(props: TouchProps) {
       ref={useComposedRefs(...props.refs)}
       onClick={(e) => {
         if (e.detail === 2) {
-          props.onDoublePress?.();
+          props.onDoublePress?.(e as unknown as GestureResponderEvent);
         } else {
-          props.onPress?.();
+          props.onPress?.(e as unknown as GestureResponderEvent);
         }
       }}
       className={`_p ${theme}`}
--- client/src/app/ui/touch/touch.types.tsx
+++ client/src/app/ui/touch/touch.types.tsx
@@ -1,6 +1,8 @@
+import type {GestureResponderEvent} from 'react-native';
+
 export interface TouchProps extends React.PropsWithChildren {
   // biome-ignore lint/suspicious/noExplicitAny: multiple ref types
   refs?: any;
-  onPress?: () => void;
-  onDoublePress?: () => void;
+  onPress?: (e: GestureResponderEvent) => void;
+  onDoublePress?: (e: GestureResponderEvent) => void;
 }
