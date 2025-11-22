719b0c922515bdb2d16c822fcd334fa72d5fc6f3
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-11-22 09:45:49 -0600

    proper device id

--- client/src/app/data/index.tsx
+++ client/src/app/data/index.tsx
@@ -1,19 +1,28 @@
+import {createMMKV} from 'react-native-mmkv';
 import {createIdFromString} from '@evolu/common';
 import {createUseEvolu, EvoluProvider} from '@evolu/react';
 import {Provider as ReduxProvider} from 'react-exo/redux';
 import {getDeviceName} from 'app/lib/platform';
-import {HfsProvider} from './lib/hfs';
-import {DeviceId} from './types';
-import evolu from './lib/evolu.db';
-import redux from './lib/redux.db';
+import {HfsProvider} from 'app/data/lib/hfs';
+import {DeviceId} from 'app/data/types';
+import {uuid} from 'app/lib/random';
+import evolu from 'app/data/lib/evolu.db';
+import redux from 'app/data/lib/redux.db';
 
 export * from '@evolu/common';
 export * from '@evolu/react';
 export * from './lib/evolu.db';
 export * from './lib/redux.db';
 
+const _mmkv = createMMKV({id: 'evolu-device'});
+let _deviceId = _mmkv.getString('deviceId');
+if (!_deviceId) {
+  _deviceId = createIdFromString(uuid());
+  _mmkv.set('deviceId', _deviceId);
+}
+
 export const useEvolu = createUseEvolu(evolu);
-export const deviceId = DeviceId.orThrow(createIdFromString('device-1'));
+export const deviceId = DeviceId.orThrow(_deviceId);
 export const deviceName = getDeviceName();
 export {evolu};
 
