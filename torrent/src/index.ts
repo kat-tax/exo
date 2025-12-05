98a0c7b68ced17dd2544e2ec5c18ea4fc3417bcc
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-12-05 13:29:02 -0600

    Update index.ts

--- torrent/src/index.ts
+++ torrent/src/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import WT from 'webtorrent/dist/webtorrent.min.js';
-import type {WebTorrent} from 'webtorrent';
+import type {WebTorrent, Torrent, TorrentFile} from 'webtorrent';
 
 export default <WebTorrent>WT;
-export type {WebTorrent};
+export type {WebTorrent, Torrent, TorrentFile};
